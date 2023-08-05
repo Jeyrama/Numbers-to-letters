6034def023a9a7bfc5226a3647b285ef179a150a
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-08-04 19:32:43 -0700

    Update Numbers-to-letters.js

--- Numbers-to-letters.js
+++ Numbers-to-letters.js
@@ -16,3 +16,12 @@ const alpha = ' zyxwvutsrqponmlkjihgfedcba!? '
 const switcher = x => x.map(item => alpha[item]).join('')
 
 // or
+
+function switcher(x) {
+  return x.map(a => {
+    if(a == 27) return "!";
+    if(a == 28) return "?";
+    if(a == 29) return " ";
+    return String.fromCharCode(97 + 26 - parseInt(a));
+  }).join('');
+}
